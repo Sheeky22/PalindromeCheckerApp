7b62038b13a3a14d343fdcf8129e60bf8393e7ed
Sheeky22 <sree22sathya@gmail.com>
2026-02-18 15:52:00 +0530

    Two Pointer Method

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,13 +1,19 @@
 public class PalindromeCheckerApp {
     public static void main(String[] args){
-        String str1 = "madam";
-        System.out.println("Input String: "+str1);
-        String strReverse = "";
-        for(int i=str1.length()-1;i>=0;i--){
-            strReverse+=str1.charAt(i);
+        String input = "sree";
+        System.out.println("Input String: "+input);
+        char[] inputChar = input.toCharArray();
+        int start =0,end=inputChar.length-1;
+        boolean isPalindrome = true;
+        while(start<end){
+            if(inputChar[start]!=inputChar[end]){
+                isPalindrome=false;
+                break;
+            }
+            start++;
+            end--;
         }
-        System.out.println(strReverse);
-        System.out.println("Is it a Palindrome? "+(str1.equals(strReverse)?"True":"False"));
+        System.out.println("Is it a Palindrome? "+((isPalindrome)?"True":"False"));
 
 
     }
