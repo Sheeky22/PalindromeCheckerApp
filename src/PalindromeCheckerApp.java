e9207f1ed37855b0d2de8767b3e562008f3c2a83
Sheeky22 <sree22sathya@gmail.com>
2026-02-18 16:11:55 +0530

    Queue+Stack Method

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,16 +1,20 @@
 import java.util.Stack;
+import java.util.Queue;
+import java.util.LinkedList;
 
 public class PalindromeCheckerApp {
     public static void main(String[] args){
         String input = "madam";
         System.out.println("Input String: "+input);
         Stack<Character> stack = new Stack<>();
+        Queue<Character> queue = new LinkedList<>();
         boolean isPalindrome = true;
         for(char c: input.toCharArray()){
             stack.push(c);
+            queue.add(c);
         }
-        for(char c: input.toCharArray()){
-            if(c!=stack.pop()){
+        while(!queue.isEmpty()){
+            if(queue.remove()!=stack.pop()){
                 isPalindrome=false;
                 break;
             }
