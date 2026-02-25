4b674f06e019e57190d08a72d4e4016b3a12ea00
Sheeky22 <sree22sathya@gmail.com>
2026-02-25 15:01:04 +0530

    Recursive Implementation

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,20 +1,27 @@
 import java.util.*;
 
 public class PalindromeCheckerApp {
+
+    private static boolean check(String s,int start, int end){
+        if(start>=end){
+            return true;
+        }
+        else if (s.charAt(start)!=s.charAt(end))            {
+            return false;
+        }
+        else{
+              start++;
+              end--;
+              return check(s,start,end);
+        }
+    }
+
     public static void main(String[] args){
+
         String input = "madam";
         System.out.println("Input String: "+input);
-        LinkedList<Character> ll = new LinkedList<>();
-        boolean isPalindrome = true;
-        for(char c: input.toCharArray()){
-            ll.addLast(c);
-        }
-        while(ll.size()>1){
-            if(ll.poll()!=ll.pollLast()){
-                isPalindrome=false;
-                break;
-            }
-        }
+        boolean isPalindrome = check(input,0,input.length()-1);
+
         System.out.println("Is it a Palindrome? "+((isPalindrome)?"True":"False"));
 
 
