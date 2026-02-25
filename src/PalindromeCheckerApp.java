8df05e69bfd0996f0c59f26fb7e03aee2fc76d00
Sheeky22 <sree22sathya@gmail.com>
2026-02-25 15:46:03 +0530

    OOPS Implementation

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -2,30 +2,39 @@ import java.util.*;
 
 public class PalindromeCheckerApp {
 
-    private static boolean check(String s,int start, int end){
-        if(start>=end){
-            return true;
+    static class PalindromeService{
+
+        private boolean check(String s,int start, int end){
+            if(start>=end){
+                return true;
+            }
+            else if (s.charAt(start)!=s.charAt(end))            {
+                return false;
+            }
+            else{
+                start++;
+                end--;
+                return check(s,start,end);
+            }
         }
-        else if (s.charAt(start)!=s.charAt(end))            {
-            return false;
+
+        public void checkPalindrome(String input){
+            System.out.println("Input String: "+input);
+
+            boolean isPalindrome = check(input,0,input.length()-1);
+
+            System.out.println("Is it a Palindrome? "+((isPalindrome)?"True":"False"));
         }
-        else{
-              start++;
-              end--;
-              return check(s,start,end);
-        }
+        
     }
+
+
 
     public static void main(String[] args){
 
-        String input = "A man a plan a canal Panama";
-        System.out.println("Input String: "+input);
-        String normalisedInput = input.replaceAll("\\s+","").toLowerCase();
-        System.out.println("Normalised String: "+normalisedInput);
-
-        boolean isPalindrome = check(normalisedInput,0,normalisedInput.length()-1);
-
-        System.out.println("Is it a Palindrome? "+((isPalindrome)?"True":"False"));
+        PalindromeService str1 = new PalindromeService();
+        str1.checkPalindrome("madam");
+        str1.checkPalindrome("sreekesh");
 
 
     }
