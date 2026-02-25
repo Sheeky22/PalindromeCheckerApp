de1beb7ed677d1e4a71aebf15af6ce48e457ad14
Sheeky22 <sree22sathya@gmail.com>
2026-02-25 16:44:19 +0530

    Performance Comparison

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -49,12 +49,28 @@ public class PalindromeCheckerApp {
 
     public static void main(String[] args){
 
-        PalindromeStrategy stackStr = new stackStrategy();
-        PalindromeStrategy queueStr = new dequeStrategy();
-        boolean isPalindrome = stackStr.checkPalindrome("madam");
-        System.out.println("Is it a Palindrome? "+((isPalindrome)?"True":"False"));
-        isPalindrome = queueStr.checkPalindrome("sreekesh");
-        System.out.println("Is it a Palindrome? "+((isPalindrome)?"True":"False"));
+        String input = "saippuakivikauppias";
+        PalindromeStrategy[] strategies = { new stackStrategy(), new dequeStrategy() };
+
+        System.out.println("--- Algorithm Comparison ---");
+        System.out.printf("%-20s | %-15s | %-20s%n", "Strategy", "Result", "Time (ns)");
+        System.out.println("------------------------------------------------------------");
+
+        // 2. Execution Loop
+        for (PalindromeStrategy s : strategies) {
+            long start = System.nanoTime();
+
+            boolean isPalindrome = s.checkPalindrome(input);
+
+
+            long end = System.nanoTime();
+            long duration = end - start;
+
+            System.out.println("Is it a Palindrome? " + ((isPalindrome) ? "True" : "False"));
+
+            String strategyName = s.getClass().getSimpleName();
+            System.out.printf("%-20s | %-15b | %-20d%n", strategyName, isPalindrome, duration);
+        }
 
     }
 }
